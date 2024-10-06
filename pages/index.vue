97a942d85fe0fba963407b8675ce3e513b910033
Rasmus P <djrasmusp@users.noreply.github.com>
2024-10-06 17:17:22 +0200

    added extra styling

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,13 @@
       <div>
         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
         <div class="mt-2">
-          <input type="email" name="email" id="email" v-model="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="you@example.com" />
+          <input type="email" name="email" id="email" v-model="email" class="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="you@example.com" />
         </div>
       </div>
       <div>
         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
         <div class="mt-2">
-          <input type="password" name="email" id="email" v-model="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="password" />
+          <input type="password" name="email" id="email" v-model="password" class="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="password" />
         </div>
       </div>
       <button type="submit" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</button>
@@ -21,7 +21,7 @@
       <table class="min-w-full divide-y divide-gray-300 mb-8">
         <thead class="bg-gray-50">
         <tr class="divide-x divide-gray-300">
-          <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Description</th>
+          <th scope="col" class="py-3.5 pl-4 w-96 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Description</th>
           <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Monday</th>
           <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Tuesday</th>
           <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Wedsday</th>
@@ -33,7 +33,7 @@
         </thead>
         <tr v-for="entry in groupByDescriptionAndWeek(getEntries(project.id ))" class="even:bg-gray-50 divide-x">
           <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">{{ entry.description }}</td>
-          <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 tabular-nums" v-for="day in entry.dates" align="right">{{ day.duration ? parseFloat(day.duration).toFixed(2) : '-' }}</td>
+          <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 tabular-nums" :class="{ 'text-center text-gray-400' : !day.duration }" v-for="day in entry.dates" align="right">{{ day.duration ? parseFloat(day.duration).toFixed(2) : '-' }}</td>
         </tr>
 
       </table>
