89675748408812891a7ab4cda65b61e5fe825d6c
Rasmus P <djrasmusp@users.noreply.github.com>
2024-10-31 20:01:51 +0100

    hide datepicker on first load

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
       <button type="submit" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</button>
     </form>
 
-    <div class="mx-auto my-8 max-w-sm">
+    <div class="mx-auto my-8 max-w-sm" v-if="projects?.length > 0">
       <vue-date-picker v-model="dateRange" week-picker />
       <pre>{{ dateRange}}</pre>
     </div>
